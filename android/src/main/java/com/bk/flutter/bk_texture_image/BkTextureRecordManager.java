f5487a9d884c03da9ed4214305abcadb7da57ee2
peiwei001 <peiwei001@ke.com>
2021-10-13 12:28:04 +0800

    fix: memory issues.

--- android/src/main/java/com/bk/flutter/bk_texture_image/BkTextureRecordManager.java
+++ android/src/main/java/com/bk/flutter/bk_texture_image/BkTextureRecordManager.java
@@ -2,39 +2,47 @@ package com.bk.flutter.bk_texture_image;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class BkTextureRecordManager {
 
-    private Map<String, BkTextureRecord> textureRecords = new HashMap<>();
+  private ConcurrentHashMap<String, BkTextureRecord> textureRecords = new ConcurrentHashMap<>();
 
-    BkTextureRecordManager() {
+  BkTextureRecordManager() {
 
-    }
+  }
 
-    public void add(BkTextureRecord record) {
-        textureRecords.put(record.getKey(), record);
-    }
+  public void add(BkTextureRecord record) {
+    textureRecords.put(record.getKey(), record);
+  }
 
-    public BkTextureRecord remove(BkTextureRecord record) {
-        return textureRecords.remove(record.getKey());
-    }
+  public BkTextureRecord remove(BkTextureRecord record) {
+    return textureRecords.remove(record.getKey());
+  }
 
-    public BkTextureRecord find(String key) {
-        return textureRecords.get(key);
-    }
+  public BkTextureRecord find(String key) {
+    return textureRecords.get(key);
+  }
 
 
-
-    public BkTextureRecord findUnusedById(long textureId) {
-        for (BkTextureRecord record : textureRecords.values()) {
-            if (textureId == record.getTextureId()) {
-                if (record.getRefCount().getAndDecrement() > 0) {
-                    return null;
-                }
-                return record;
-            }
+  // 返回待销毁的TextureRecord， 如果当前TextureRecord被使用数 > 1, 则不销毁返回null
+  public BkTextureRecord findUnusedById(long textureId) {
+    for (BkTextureRecord record : textureRecords.values()) {
+      if (textureId == record.getTextureId()) {
+        if (record.getRefCount().getAndDecrement() >= 1) {
+          return null;
         }
-        return null;
+        BkLogger.d("findUnusedById : -->" + record);
+        return record;
+      }
     }
+    return null;
+  }
 
+  public void clean() {
+    for (BkTextureRecord record : textureRecords.values()) {
+      record.tryRelease();
+    }
+    textureRecords.clear();
+  }
 }
